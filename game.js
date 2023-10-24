fe42aeb1e7f1e1c24f617da7fbb4d42835ce9b7c
christopherbholland <140353353+christopherbholland@users.noreply.github.com>
2023-10-24 07:50:08 -0500

    Adding random computer selection function to pick rock, paper, or scissors

--- game.js
+++ game.js
@@ -1,53 +1,78 @@
 function getComputerChoice() {
-
-}
-
-function playRound (playerSelection, computerSelection) {
-
-}
-
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection,computerSelection));
-
-let playerWin = string("You win!", playerSelection, "beats ", ComputerSelection, ".");
-let playerLose = string("You lose!", computerSelection beats playerSelection, ".");
-let playerTie = string("It's a tie");
-let playerError = string("Unknown play. Please enter Rock, Paper, or Scissors.")
-
-
-function game() {
- if (playerSelection === computerSelection) {
-    return playerTie;
- } else if {
-    if (_playerSelection    === Rock) { // evaluate player is Rock
-        if (computer = Paper) {
-            return playerLose;
-        } else if (computer = Scissors) {
-            return playerWin;
-        }
-    } else {
-    if (playerSelection === Paper) { // evaluate player is Paper
-        if (computerSelection === Rock) {
-            return playerWin;
-        if (computerSelection === Scissors) // computer wins
-            return playerLose;
-        }
-    } else {
-    if (playerSelection === Scissors) {
-        if (computerSelection === Rock) {
-            return playerLose;
-        } else if (computer === Paper) { => player wins
-            return playerWin;
-    } else {
-        return playerError;
-        }
-        }
-        }
-        }
-
-        }
+    const randomNumber = Math.floor(Math.random() * 3); // randomly pick a number
+    switch (randomNumber) {  // set the output to one of the three options
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+        default:
+            return "Unknown"; // this should never happen
     }
+}
+
+let computerSelection = getComputerChoice(); // assign the output to computer's selection
+const playerSelection = "Rock";
+
+let playerWin = ("You win!", playerSelection, "beats ", computerSelection, ".");
+let playerLose = ("You lose!", computerSelection, "beats ", playerSelection, ".");
+let playerTie = ("It's a tie");
+let playerError = ("Unknown play. Please enter Rock, Paper, or Scissors.")
+
+console.log(playerWin);
+
+
+// function playRound (playerSelection, computerSelection) {
+//     if (playerSelection === computerSelection) {
+//         return playerTie;
+//         } else if { (playerSelection == "Rock" && computerSelection == "Paper")  // evaluate player is Rock
+//             return playerLose; 
+//         } else if { (playerSelection == "Rock" && computerSelection == "Scissors")
+//             return playerWin;
+//         } else if { (playerSelection == "Paper" && computerSelection == "Rock")
+//             return playerWin;
+//         } else if { (playerSelection == "Paper" && computerSelection == "Scissors")
+//             return playerLose;
+//         } else if { (playerSelection == "Scissors" && computerSelection == "Rock")
+//             return playerLose;
+//         } else if { (playerSelection == "Scissors" && computerSelection == "Paper")
+//             return playerWin;
+//         } else {
+//             return playerError;
+//         }    
+// }
+
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection,computerSelection));
+
+
+// function game() {
+
+
+//     // } else if {
+//     // if (playerSelection = "Paper") { // evaluate player is Paper
+//     //     if (computerSelection = "Rock") {
+//     //         return playerWin;
+//     //     if (computerSelection = "Scissors") // computer wins
+//     //         return playerLose;
+//     //     }
+    // } else {
+    // if (playerSelection = "Scissors") {
+    //     if (computerSelection = "Rock") {
+    //         return playerLose;
+    //     } else if (computer = "Paper") { // player wins
+    //         return playerWin;
+    // } else {
+    //     return playerError;
+    //     }
+    //     }
+    //     }
+    //     }
+
+    //     }
+    // }
 
 
 
